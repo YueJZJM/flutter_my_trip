27cb5ce6131651697f8e38b4a57fce74fa84dded
岳剑钟 <yuejzhfut@163.com>
2020-05-31 22:32:16 +0800

    实现首页活动卡片布局

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,8 +6,9 @@ import 'package:fluttermytrip/dao/home_dao.dart';
 import 'package:fluttermytrip/model/common_module.dart';
 import 'package:fluttermytrip/model/grid_nav_module.dart';
 import 'package:fluttermytrip/model/home_module.dart';
-import 'package:fluttermytrip/widget/GridNav.dart';
+import 'package:fluttermytrip/widget/grid_nav.dart';
 import 'package:fluttermytrip/widget/local_nav.dart';
+import 'package:fluttermytrip/widget/sub_nav.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 class HomePage extends StatefulWidget {
@@ -20,6 +21,7 @@ class _HomePageState extends State<HomePage> {
   double appBarAlpha = 0;
   String resultString = "resultString";
   List<CommonModel> localNavList = [];
+  List<CommonModel> subNavList = [];
   GridNavModel gridNavModel;
 
   @override
@@ -69,7 +71,10 @@ class _HomePageState extends State<HomePage> {
                     ),
                     Padding(padding:EdgeInsets.fromLTRB(7, 4, 7, 4),
                       child: LocalNav(localNavList: localNavList),),
-                    GridNav(gridNavModel: gridNavModel),
+                    Padding(padding:EdgeInsets.fromLTRB(7, 0, 7, 4),
+                      child:  GridNav(gridNavModel: gridNavModel),),
+                    Padding(padding:EdgeInsets.fromLTRB(7, 0, 7, 4),
+                      child:  SubNav(subNavList: subNavList),),
                     Container(
                       height: 800,
                       child: ListTile(title: Text(resultString),),
@@ -124,7 +129,8 @@ class _HomePageState extends State<HomePage> {
      setState(() {
        localNavList = model.localNavList;
        gridNavModel = model.gridNav;
-       print("gridNavModel:"+gridNavModel.hotel.item1.toString());
+       subNavList = model.subNavList;
+//       print("sublist" + subNavList.toString());
      });
    }catch(e){
      setState(() {
